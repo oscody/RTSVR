1e49316360fe963200fc29a37f79fb43e4425365
oscody <bogleoneil@gmail.com>
2026-07-20 12:16:55 -0400

    feat: update development scripts and server configuration for improved workflow

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,12 @@ export default defineConfig({
 
     compileUIKit({ sourceDir: "ui", outputDir: "public/ui", verbose: true }),
   ],
-  server: { host: "0.0.0.0", port: 8081, open: true },
+  server: {
+    host: "0.0.0.0",
+    port: 8081,
+    strictPort: true,
+    open: false,
+  },
   build: {
     outDir: "dist",
     sourcemap: process.env.NODE_ENV !== "production",
